af373ead0684708d3c905d3d87d37b3491678081
unknown <urgedlion@gmail.com>
2021-04-24 20:06:24 +0530

    added tab navigation

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import FacebookScreen from './screen/fb';
+import InstaScreen from './screen/in';
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
 }
-
+const TabNavigator = createBottomTabNavigator({
+  Transaction: { screen: FacebookScreen },
+  Search: { screen: InstaScreen },
+});
+const AppContainer = createAppContainer(TabNavigator);
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ screen/in.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+export default class InstaScreen extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>InstaScreen</Text>
+            </View>);
+    }
+}
